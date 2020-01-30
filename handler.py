49229b2b1cd7de00475e991ea53445f03a59923b
tim-davenport <timdavenport81@gmail.com>
2020-01-30 21:41:28 +0000

    working version of post function

--- handler.py
+++ handler.py
@@ -1,24 +1,18 @@
 import json
+import boto3
+import os
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def post(event, context):
+    
+    dynamodb = boto3.resource('dynamodb')
+    
+    linksTable = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    linksTable.put_item(Item=json.loads(event['body']))
 
     response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
+        "statusCode": 201
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
